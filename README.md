a205d9ea330020ef864f224280991e8cd02968fc
Kej <kejjang@gmail.com>
2015-09-07 16:11:57 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,41 @@
+#Ronum
+
+Just another Roman numerals converter, input a number and get a Roman numeral string, input a Roman numeral string and get a number, easy as shit.
+
+---
+
+##Usage
+
+	<script type="text/javascript" src="ronum.js"></script>
+	<script type="text/javascript">
+		var ronum = new Ronum();
+		console.log(ronum.conv(1949));       // MCMXLIX
+		console.log(ronum.conv('MCMXLIX'));  // 1949
+	</script>
+
+---
+
+##License
+
+this software is released under [WTFPL](http://www.wtfpl.net/)
+
+	 Copyright (C) 2015 Kej Jang <kejjang@gmail.com>
+	 
+	 This work is free. You can redistribute it and/or modify it under the
+	 terms of the Do What The Fuck You Want To Public License, Version 2,
+	 as published by Sam Hocevar. See below for more details.
+	 
+	 
+	            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+	                    Version 2, December 2004
+	 
+	 Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+	 
+	 Everyone is permitted to copy and distribute verbatim or modified
+	 copies of this license document, and changing it is allowed as long
+	 as the name is changed.
+	 
+	            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+	   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+	 
+	  0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ ronum.js
@@ -0,0 +1,122 @@
+function Ronum(){
+    this.min = 1;
+    this.max = 4999;
+    this.idxNumberToRoman = [];
+    this.idxNumberToRoman[1]    = 'I';
+    this.idxNumberToRoman[5]    = 'V';
+    this.idxNumberToRoman[10]   = 'X';
+    this.idxNumberToRoman[50]   = 'L';
+    this.idxNumberToRoman[100]  = 'C';
+    this.idxNumberToRoman[500]  = 'D';
+    this.idxNumberToRoman[1000] = 'M';
+    this.idxRomanToNumber = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+}
+
+Ronum.prototype.conv = function(num){
+    if(!+num){
+        return this.convRomanToNumber(num);
+    }else{
+        return this.convNumberToRoman(num);
+    }
+}
+
+Ronum.prototype.validNum = function(num){
+    if(!+num){
+        return false;
+    }else if(num < this.min || num > this.max){
+        return 999;
+    }else{
+        return true;
+    }
+}
+
+Ronum.prototype.validRom = function(num){
+    var digits = num.split('');
+    for(var i in digits){
+        if(!this.idxRomanToNumber.hasOwnProperty(digits[i])){
+            return false;
+        }
+    }
+    return true;
+}
+
+Ronum.prototype.convNumberToRoman = function(num){
+    var valid = this.validNum(num);
+    if(false === valid){
+        return 'not a valid number';
+    }else if(valid === 999){
+        return 'the number you give is too big or too small';
+    }
+
+    var digits = num.toString().split('');
+    var ret = [];
+    for(var i in digits){
+        var base = Math.pow(10, digits.length - 1 - i);
+        var n = parseInt(digits[i], 10);
+
+        if(n % 5 == 4){
+            if(this.idxNumberToRoman[base * (n + 1)] === undefined){
+                ret[i] = Array(n + 1).join(this.idxNumberToRoman[base]);
+            }else{
+                ret[i] = this.idxNumberToRoman[base] + this.idxNumberToRoman[base * (n + 1)];
+            }
+        }else{
+            ret[i] = Array(Math.floor(n / 5) + 1).join(this.idxNumberToRoman[base * 5]) + Array((n + 1) % 5).join(this.idxNumberToRoman[base]);
+        }
+    }
+    return ret.join('');
+}
+
+Ronum.prototype.convRomanToNumber = function(num){
+    var valid = this.validRom(num);
+    if(false === valid){
+        return 'not a valid roman';
+    }else if(valid === 999){
+        return 'the number you give is too big or too small';
+    }
+
+    var digits = num.split('');
+    var valid2 = true;
+
+    for(var i in digits){
+        digits[i] = this.idxRomanToNumber[digits[i]];
+        if(i > 0){
+            if(digits[i] > digits[i - 1]){
+                if(digits[i] / digits[i - 1] == 10 || digits[i] / digits[i - 1] == 5){
+                    digits[i - 1] *= -1;
+                }else{
+                    valid2 = false;
+                }
+            }else{
+                for(var j = 0; j < i; j++){
+                    if(digits[j] == digits[i] * -1){
+                        valid2 = false;
+                    }
+                }
+            }
+        }
+    }
+
+    if(false === valid2){
+        return 'not a valid roman';
+    }
+    return digits.sum();
+}
+
+// array utils
+Array.prototype.sum = function(){
+    var __sum = 0;
+    for(var i = 0; i < this.length; i++){
+        __sum += this[i];
+    }
+    return __sum;
+}
+Object.defineProperty(Array.prototype, 'sum', {enumerable: false});
